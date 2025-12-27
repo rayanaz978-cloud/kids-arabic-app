d3f74b77c7676aabe52544e10088bffc6a92b9f5
rayanaz978-cloud <rayanaz978@gmail.com>
2025-12-27 09:55:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function playSound(letter) {
   const audio = new Audio(`assets/sounds/${letter}.mp3`);
   audio.play();
 }
+
+// إخفاء الدعاء بعد 3 ثوانٍ
+window.onload = function () {
+  setTimeout(() => {
+    document.getElementById("splash").style.display = "none";
+    document.getElementById("app").classList.remove("hidden");
+  }, 3000); // 3000 = 3 ثوانٍ
+};
